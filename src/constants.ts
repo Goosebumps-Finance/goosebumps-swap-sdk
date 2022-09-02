092f314b99e39008e466d0a4f58e30c225685267
CryptoSnowPrince <topdirector2017@gmail.com>
2022-09-02 04:13:46 +0200

    update lpfee value

--- src/constants.ts
+++ src/constants.ts
@@ -31,7 +31,7 @@ export const THREE = JSBI.BigInt(3)
 export const FIVE = JSBI.BigInt(5)
 export const TEN = JSBI.BigInt(10)
 export const _100 = JSBI.BigInt(100)
-export const _997 = JSBI.BigInt(997)
+export const _999 = JSBI.BigInt(999)
 export const _1000 = JSBI.BigInt(1000)
 
 export enum SolidityType {
