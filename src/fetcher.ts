c4e82162915b1c2edb621d36972251a26a229d46
CryptoSnowPrince <topdirector2017@gmail.com>
2022-09-02 04:24:58 +0200

    fix TOKEN_DECIMALS_CACHE and publish 0.0.9

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -11,6 +11,8 @@ import { Token } from './entities/token'
 
 let TOKEN_DECIMALS_CACHE: { [chainId: number]: { [address: string]: number } } = {
   [ChainId.MAINNET]: {
+  },
+  [ChainId.ETHEREUM]: {
     '0xE0B7927c4aF23765Cb51314A0E0521A9645F0E2A': 9 // DGD
   }
 }
